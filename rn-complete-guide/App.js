415f7d5e05108bab4687421aa2f0fb079c59f6ce
irfan <engr.irfandayan@gmail.com>
2022-10-24 16:37:05 +0500

    add new react app

---
+++ rn-complete-guide/App.js
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{ padding: 50 }}>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <TextInput
+          placeholder="Course Goal"
+          style={{
+            width: '80%',
+            borderBottomColor: 'black',
+            borderWidth: 1,
+            padding: 10,
+          }}
+        />
+        <Button title="ADD" />
+      </View>
+      <View></View>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
